629c296889318921e0700dd8adbbeb8a072df1f9
Mikhail Gorbatov <adgjwpt25@gmail.com>
2022-02-10 10:48:32 +0300

    input without check

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,31 @@
 import defoult.SpaceMarine;
 
-import java.io.FileNotFoundException;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.util.TreeMap;
 
 public class Main {
-     public static void main(String[] args){
-         String filename;
+    public static void main(String[] args) {
+        String filename;
+        Map<Integer, SpaceMarine> kek = new TreeMap<Integer, SpaceMarine>();
+        kek.put(123, new SpaceMarine());
+        Scanner scanner = new Scanner(System.in);
+        String s;
+        s = scanner.nextLine();
+        while (!Objects.equals(s, "exit")) {
 
-//         try{
-//             filename=args[0];
-//         } catch (Exception e) {
-//             System.out.println("Не выбран файл");
-//             return;
-//         }
-//         try{
-//             FileInputStream inputStream = new FileInputStream(filename);
-//         } catch (FileNotFoundException e) {
-//             System.out.println("Файл не существует");
-//             return;
-//         }
-//
-         Map<Integer, SpaceMarine> kek = new TreeMap<Integer, SpaceMarine>();
-         kek.put(123, new SpaceMarine());
-         Scanner scanner=new Scanner(System.in);
-         String s;
-         s = scanner.nextLine();
-         
+            String[] curr = s.split(" ");
+            switch (curr[0]) {
+                case ("insert"):
+                    Integer id = Integer.getInteger(curr[1]);
+                    SpaceMarine temp = new SpaceMarine();
+
+
+            }
+
+            s = scanner.nextLine();
+        }
+
     }
 }
