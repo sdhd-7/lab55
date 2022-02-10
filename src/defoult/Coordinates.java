ef735e21483aa5249c3aa26a45abdea5596dfd6f
Mikhail Gorbatov <adgjwpt25@gmail.com>
2022-02-10 11:38:44 +0300

    setter and construct

--- src/defoult/Coordinates.java
+++ src/defoult/Coordinates.java
@@ -3,4 +3,17 @@ package defoult;
 public class Coordinates {
     private Integer x; //Поле не может быть null
     private double y;
+
+    public Coordinates(Integer x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
