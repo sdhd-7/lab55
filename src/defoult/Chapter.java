1a658b22e7dc860fb805c51847f75a8933a31019
Mikhail Gorbatov <adgjwpt25@gmail.com>
2022-02-10 12:11:46 +0300

    setter and construct

--- src/defoult/Chapter.java
+++ src/defoult/Chapter.java
@@ -3,4 +3,17 @@ package defoult;
 public class Chapter {
     private String name; //Поле не может быть null, Строка не может быть пустой
     private int marinesCount; //Значение поля должно быть больше 0, Максимальное значение поля: 1000
+
+    public Chapter(String name, int marinesCount) {
+        this.marinesCount = marinesCount;
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMarinesCount(int marinesCount) {
+        this.marinesCount = marinesCount;
+    }
 }
