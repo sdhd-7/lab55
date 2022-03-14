20def01eea9b15b86061203fb33f6fcd14bdc462
Mikhail Gorbatov <adgjwpt25@gmail.com>
2022-03-14 20:31:47 +0300

    Добавлены исключения для неверно введенного типа данных.

--- src/programm/helper/Reception.java
+++ src/programm/helper/Reception.java
@@ -33,55 +33,107 @@ public class Reception {
                 case "":
                     break;
                 case "help":
+                    if (scom.length != 1) {
+                        maker.wrong();
+                        break;
+                    }
                     maker.help();
                     break;
                 case "exit":
+                    if (scom.length != 1) {
+                        maker.wrong();
+                        break;
+                    }
                     maker.exit();
                     break;
                 case "clear":
+                    if (scom.length != 1) {
+                        maker.wrong();
+                        break;
+                    }
                     maker.clear();
                     break;
                 case "add":
+                    if (scom.length != 1) {
+                        maker.wrong();
+                        break;
+                    }
                     maker.add(scanner);
                     break;
                 case "info":
                     maker.info();
                     break;
                 case "update":
-                    maker.update(Long.parseLong(scom[1]), scanner);
+                    try {
+                        maker.update(Long.parseLong(scom[1]), scanner);
+                    } catch (Exception ex) {
+                        maker.wrong();
+                    }
                     break;
                 case "remove_by_id":
-                    maker.remove_by_id(Long.parseLong(scom[1]));
+                    try {
+                        maker.remove_by_id(Long.parseLong(scom[1]));
+                    } catch (Exception ex) {
+                        maker.wrong();
+                    }
                     break;
                 case "execute_script":
+                    if (scom.length != 1) {
+                        maker.wrong();
+                        break;
+                    }
                     maker.execute_script(scom[1]);
                     break;
                 case "add_if_max":
+                    if (scom.length != 1) {
+                        maker.wrong();
+                        break;
+                    }
                     maker.add_if_max(scanner);
                     break;
                 case "remove_all_by_type":
                     maker.remove_all_by_type(scom[1]);
                     break;
                 case "count_by_age":
-                    maker.count_by_age(Integer.parseInt(scom[1]));
+                    try {
+                        maker.count_by_age(Integer.parseInt(scom[1]));
+                    } catch (Exception ex) {
+                        maker.wrong();
+                    }
                     break;
                 case "filter_by_character":
                     maker.filter_by_character(scom[1]);
                     break;
                 case "show":
+                    if (scom.length != 1) {
+                        maker.wrong();
+                        break;
+                    }
                     maker.show();
                     break;
                 case "save":
+                    if (scom.length != 1) {
+                        maker.wrong();
+                        break;
+                    }
                     maker.save();
                     break;
                 case "remove_first":
+                    if (scom.length != 1) {
+                        maker.wrong();
+                        break;
+                    }
                     maker.remove_first();
                     break;
                 case "history":
+                    if (scom.length != 1) {
+                        maker.wrong();
+                        break;
+                    }
                     maker.history();
                     break;
                 default:
-                    System.out.println("Введена неверная команда, чтобы ознакомиться со списком доступных команд, введите команду help");
+                    maker.wrong();
                     break;
             }
         }
